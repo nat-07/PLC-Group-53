39ded47fa292d517012035a88e21aa2cd7918cec
nat-07 <natashaudani05@gmail.com>
2025-04-21 11:37:08 +0100

    Changed structure of Intepreter and added functionality to getting a column, and writing to output.txt

--- csvdsl/src/Interpreter.hs
+++ csvdsl/src/Interpreter.hs
@@ -2,75 +2,177 @@ module Interpreter where
 
 import Grammar
 
-interpret :: Exp2 -> IO ()
-interpret expr = case expr of
-    SELECT x         -> putStrLn "Running SELECT" >> interpret x
-    DO x             -> putStrLn "Running DO" >> interpret x
-    GET col csv      -> putStrLn $ "Getting: " ++ show col ++ " from " ++ show csv
-    FILENAME name    -> putStrLn $ "Filename: " ++ name
-    COLUMN n         -> putStrLn $ "Column Index: " ++ show n
-    STR s            -> putStrLn $ "String: " ++ s
-    ANDSELECT x y    -> do
-        putStrLn "Selecting the following items together:"
-        interpret x  -- Interpret the first select item
-        interpret y  -- Interpret the second select item
-        putStrLn "Done selecting."
-    THENTASK x y    -> do
-        putStrLn "Doing tasks in order:"
-        interpret x  -- Interpret the first select item
-        interpret y  -- Interpret the second select item
-        putStrLn "Tasks done."
-    PRODUCT x y    -> do
-        putStrLn "Running PRODUCT Task:"
-        interpret x  -- Interpret the first select item
-        interpret y  -- Interpret the second select item
-        putStrLn "Combing the two tables together"
-    DROP col file    -> putStrLn $ "Running DO Task: Dropping column" ++ show col ++ " in " ++ show file
-    PERMUTE x y    -> do 
-        putStrLn "Running PERMUTE Task:"
-        interpret x  -- Interpret the first select item
-        interpret y  -- Interpret the second select item
-        putStrLn "Permutation finished"
-    COPY file string       -> do 
-        putStrLn "Running COPY Task:"
-        interpret file  -- Interpret the first select item
-        interpret string  -- Interpret the second select item
-        putStrLn "Copy finished"
-    COPYIN col file string       -> do 
-        putStrLn "Running COPY Task:"
-        interpret col  -- Interpret the first select item
-        interpret file  -- Interpret the second select item
-        interpret string
-        putStrLn "copy finished"
-    LEFTMERGEON file1 file2 col -> do 
-        putStrLn "Running LEFT MERGE Task:"
-        interpret file1  -- Interpret the first select item
-        interpret file2  -- Interpret the second select item
-        interpret col
-        putStrLn "LEFT MERGE finished"
-    LEFTMERGECSV file1 file2 -> do
-        putStrLn "Running LEFT MERGE Task:"
-        interpret file1  -- Interpret the first select item
-        interpret file2  -- Interpret the second select item
-        putStrLn "LEFT MERGE finished"
-    LEFTMERGE file1 file2 col -> do
-        putStrLn "Running LEFT MERGE Task:"
-        interpret file1  -- Interpret the first select item
-        interpret file2  -- Interpret the second select item
-        interpret col
-        putStrLn "LEFT MERGE finished"
-    DOWHERE tasks condition -> do
-        interpret tasks  -- Interpret the first select item
-        putStrLn "with Condition:"
-        interpret condition
-        putStrLn "Tasks done"
-    SELECTWHERE selects condition -> do
-        interpret selects  -- Interpret the first select item
-        putStrLn "with Condition:"
-        interpret condition
-        putStrLn "Conditions applied"
-    CONDITIONEQITEM x y -> do 
-        putStrLn "Condition that the following are equal to each other"
-        interpret x  -- Interpret the first select item
-        interpret y  -- Interpret the second select item
-    _                -> putStrLn $ "Unimplemented: " ++ show expr
+import Data.List.Split (splitOn)
+
+interpret :: Exp2 -> IO String
+
+-- SELECT -- 
+interpret (SELECT x) = do
+    putStrLn "Running SELECT"
+    column <- interpret x
+    writeFile "output.txt" column
+    putStrLn "Wrote result to output.txt"
+    return column
+
+-- DO -- 
+interpret (DO task) = do
+    putStrLn "Running DO"
+    interpret task
+
+
+
+-- GET --
+interpret (GET col csvExpr) = do
+    putStrLn "Running GET"
+    filename <- interpret csvExpr
+    contents <- readFile filename
+    let rows = map (splitOn ",") (lines contents)
+    let colIndex = getColumnIndex col
+    let colValues = [ row !! colIndex | row <- rows, length row > colIndex ]
+    putStrLn ("Successfully got values: " ++ show colValues)
+    return $ unlines colValues
+
+interpret (FILENAME name) = return name
+
+-- STR -- 
+interpret (STR s) =
+    return $ "String: " ++ s
+
+-- ANDSELECT --
+-- Left Most Inner Most Associativity 
+interpret (ANDSELECT x y) = do
+    putStrLn "Selecting the following items together:"
+    interpret x
+    interpret y
+    return "Done selecting."
+
+-- THENTASK --
+-- Left Most Inner Most Associativity 
+interpret (THENTASK tasklist task) = do
+    putStrLn "Doing tasks in order:"
+    interpret tasklist
+    interpret task
+    return "Tasks done."
+
+-- PRODUCT --
+interpret (PRODUCT x y) = do
+    putStrLn "Running PRODUCT Task:"
+    interpret x
+    interpret y
+    return "Combing the two tables together"
+
+-- DROP --
+interpret (DROP col file) =
+    return $ "Running DROP Task: Dropping " ++ show col ++ " in " ++ show file
+
+-- PERMUTE -- 
+interpret (PERMUTE x y) = do 
+    return "Running PERMUTE Task:"
+    interpret x
+    interpret y
+    return "Permutation finished"
+
+-- COPY a string to a file --
+interpret (COPY file string) = do 
+    return "Running COPY Task:"
+    interpret file
+    interpret string
+    return "Copy finished"
+
+-- COPYIN a string to a file of a speciifc column  --
+interpret (COPYIN col file string) = do 
+    return "Running COPY Task:"
+    interpret col
+    interpret file
+    interpret string
+    return "copy finished"
+
+-- COPYIN a string to a file of a speciifc column  --
+interpret (LEFTMERGEON file1 file2 col) = do 
+    return "Running LEFT MERGE Task:"
+    interpret file1
+    interpret file2
+    interpret col
+    return "LEFT MERGE finished"
+
+-- LEFT MERGE two files
+interpret (LEFTMERGECSV file1 file2) = do
+    return "Running LEFT MERGE Task:"
+    interpret file1
+    interpret file2
+    return "LEFT MERGE finished"
+
+-- LEFT MERGE two files of a specific column --
+interpret (LEFTMERGE file1 file2 col) = do
+    return "Running LEFT MERGE Task:"
+    interpret file1
+    interpret file2
+    interpret col
+    return "LEFT MERGE finished"
+
+-- Conditions for tasks --
+interpret (DOWHERE tasks condition) = do
+    interpret tasks
+    return "with Condition:"
+    interpret condition
+    return "Conditions applied"
+
+-- Conditions for Select statements --
+interpret (SELECTWHERE selects condition) = do
+    interpret selects
+    return "with Condition:"
+    interpret condition
+    return "Conditions applied"
+
+-- Conditions when equalling two items together --
+interpret (CONDITIONEQITEM item1 item2) = do 
+    return "Condition that the following are equal to each other"
+    interpret item1
+    interpret item2
+
+-- Conditions an item is equal to a particular string --
+interpret (CONDITIONEQSTRING item1 item2) = do
+    return "Condition that the following are equal to each other"
+    interpret item1
+    interpret item2
+
+-- Conditions an item is NOT equal to a particular string  --
+interpret (CONDITIONNEQSTRING item1 item2) = do
+    return "Condition that the following are not equal to each other"
+    interpret item1
+    interpret item2
+
+-- Conditions an item is empty  --
+interpret (CONDITIONISEMPTY item) = do
+    return "Condition that the following is empty"
+    interpret item
+
+-- Conditions an item is not empty  --
+interpret (CONDITIONISNOTEMPTY item) = do
+    return "Condition that the following is not empty"
+    interpret item
+
+-- AND  --
+interpret (AND cond1 cond2) = do
+    interpret cond1
+    return "AND"
+    interpret cond2
+-- OR  --
+interpret (OR cond1 cond2) = do
+    interpret cond1
+    return "OR"
+    interpret cond2
+
+interpret (RESULTOF task) = do
+    interpret task
+
+
+
+
+
+
+interpret expr = return $ "Unimplemented: " ++ show expr
+
+getColumnIndex :: Exp2 -> Int
+getColumnIndex (COLUMN i) = i-1
+getColumnIndex _ = error "Expected a COLUMN expression with an index."
--- csvdsl/src/testing53.hs
+++ csvdsl/src/testing53.hs
@@ -9,19 +9,28 @@ import System.Environment
 import Control.Exception
 import System.IO
 
+import System.Environment (getArgs)
+import System.IO (hPutStr, stderr)
+import Control.Exception (catch, ErrorCall)
 
-main :: IO ()
+-- Main program
+main :: IO String
 main = catch main' noParse
 
-main' = do (fileName : _ ) <- getArgs 
+-- Main logic
+main' :: IO String
+main' = do
+    (fileName:_) <- getArgs
+    sourceText <- readFile fileName
+    putStrLn ("Parsing: " ++ sourceText)
+    let parsedProg = parseCalc (alexScanTokens sourceText)
+    putStrLn ("Parsed as: " ++ show parsedProg)
+    result <- interpret parsedProg
+    return $ show result
 
-           sourceText <- readFile fileName
-           putStrLn ("Parsing : " ++ sourceText)
-           let parsedProg = parseCalc (alexScanTokens sourceText)
-           putStrLn ("Parsed as " ++ (show parsedProg))
-           interpret parsedProg 
-
-noParse :: ErrorCall -> IO ()
-noParse e = do let err =  show e
-               hPutStr stderr err
-               return ()
+-- Error handler for parsing issues
+noParse :: ErrorCall -> IO String
+noParse e = do
+    let err = "Error occurred: " ++ show e  -- Add context to the error
+    hPutStr stderr err                    -- Print to stderr for error output
+    return err
